25be50d4256a484e534b0f35ffbe8482931b5582
ifabregat <ifabregat@fi.uba.ar>
2025-09-03 16:10:19 -0300

    Agrego pruebas para la obtencion del sub-comando

--- test/ledger_test.exs
+++ test/ledger_test.exs
@@ -1,8 +1,18 @@
 defmodule LedgerTest do
   use ExUnit.Case
   doctest Ledger
+  alias Ledger.Parser, as: Parser
 
-  test "greets the world" do
-    assert Ledger.hello() == :world
+  test "Comprobar sub-comando validos" do
+    arg = ["transacciones"]
+    assert {:ok, "transacciones"} = Parser.obtener_subcomando(arg)
+
+    arg = ["balance"]
+    assert {:ok, "balance"} = Parser.obtener_subcomando(arg)
+  end
+
+  test "Comprobar sub-comando invalidos" do
+    arg = ["otra cosa"]
+    assert {:error, "sub comando desconocido"} = Parser.obtener_subcomando(arg)
   end
 end
