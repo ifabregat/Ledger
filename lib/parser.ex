8abfeda7ba890f3e3b41e6f135a8d89d44244425
ifabregat <ifabregat@fi.uba.ar>
2025-09-03 20:06:43 -0300

    Agrego funcion para obtener parametros extras

--- lib/parser.ex
+++ lib/parser.ex
@@ -10,4 +10,33 @@ defmodule Ledger.Parser do
       _ -> {:error, "sub comando desconocido"}
     end
   end
+
+  def obtener_flags(flags) do
+    # flags = ["-c2=userA", "-c1=userB"]
+
+    flags = Enum.map(flags, fn
+      "-" <> rest -> "--" <> rest
+      other -> other
+    end)
+
+    # flags = ["--c2=userA", "--c1=userB"]
+
+    {opciones, _resto, invalidos} = OptionParser.parse(flags,
+      strict: [t: :string, o: :string, c1: :string, c2: :string, m: :string]
+    )
+
+    if invalidos != [] do
+      {:error, "parametro invalido"}
+    else
+      mapa = %{
+      archivo_input: opciones[:t] || "test/fixtures/transacciones.csv",
+      archivo_output: opciones[:o],
+      cuenta_origen: opciones[:c1],
+      cuenta_destino: opciones[:c2],
+      moneda: opciones[:m]
+      }
+
+      {:ok, mapa}
+    end
+  end
 end
