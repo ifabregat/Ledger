3b90a75c134700cc4d321c115f2edff3b1210e7b
ifabregat <ifabregat@fi.uba.ar>
2025-09-03 16:37:26 -0300

    Agrego pattern matcheo simple para argumentos

--- lib/main.ex
+++ lib/main.ex
@@ -1,5 +1,9 @@
 defmodule ExampleApp.CLI do
+  alias Ledger.Parser, as: Parser
+
   def main(args \\ []) do
-    IO.inspect(args)
+    [sub_comando | flags] = args
+    Parser.obtener_subcomando(sub_comando) |> IO.inspect()
+    Parser.obtener_flags(flags) |> IO.inspect()
   end
 end
