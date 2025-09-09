b10ed5f93fb75b88c2660a0aabce64649222f174
ifabregat <ifabregat@fi.uba.ar>
2025-09-09 20:52:38 -0300

    Agrego funcion validar subcomando y sus pruebas

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,3 +1,9 @@
-defmodule Parser do
-
+defmodule Ledger.Parser do
+  def validar_subcomando(subcomando) do
+    case subcomando do
+      "transacciones" -> {:ok, :transacciones}
+      "balance" -> {:ok, :balance}
+      _ -> {:error, :subcomando_invalido}
+    end
+  end
 end
--- test/ledger_test.exs
+++ test/ledger_test.exs
@@ -3,4 +3,9 @@ defmodule LedgerTest do
   doctest Ledger
   alias Ledger.Parser, as: Parser
 
+  test "Validar subcomandos" do
+    assert {:ok, :transacciones} == Parser.validar_subcomando("transacciones")
+    assert {:ok, :balance} == Parser.validar_subcomando("balance")
+    assert {:error, :subcomando_invalido} == Parser.validar_subcomando("otro")
+  end
   end
