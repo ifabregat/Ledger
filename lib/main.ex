c63c1a75c309125fefc7e622791e2d13af40128c
ifabregat <ifabregat@fi.uba.ar>
2025-09-09 20:16:32 -0300

    Crisis numero 1

--- lib/main.ex
+++ lib/main.ex
@@ -2,8 +2,6 @@ defmodule ExampleApp.CLI do
   alias Ledger.Parser, as: Parser
 
   def main(args \\ []) do
-    [sub_comando | flags] = args
-    Parser.obtener_subcomando(sub_comando) |> IO.inspect()
-    Parser.obtener_flags(flags) |> IO.inspect()
+
   end
 end
