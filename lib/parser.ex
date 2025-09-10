def87abdd5bae5ff858740c4155c31bd869c0793
ifabregat <ifabregat@fi.uba.ar>
2025-09-09 23:01:28 -0300

    Agrego funcion validar opciones y sus pruebas

--- lib/parser.ex
+++ lib/parser.ex
@@ -6,4 +6,36 @@ defmodule Ledger.Parser do
       _ -> {:error, :subcomando_invalido}
     end
   end
+
+  def validar_opciones(opciones, subcomando) do
+    # opciones = ["-c1=userA", "-c2=userB"]
+
+    opciones = Enum.map(opciones,
+    fn "-" <> resto -> "--" <> resto
+    other -> other end)
+
+    # opciones = ["--c1=userA", "--c2=userB"]
+
+    {opciones, resto, invalidos} = OptionParser.parse(opciones,
+      strict: [t: :string, o: :string, c1: :string, c2: :string, m: :string])
+
+    case {resto, invalidos} do
+      {[], []} ->
+        mapa_opciones = %{
+          archivo_input: opciones[:t] || "test/fixtures/transacciones.csv",
+          archivo_output: opciones[:o],
+          cuenta_origen: opciones[:c1],
+          cuenta_destino: opciones[:c2],
+          moneda: opciones[:m]
+        }
+
+        case {subcomando, mapa_opciones.cuenta_origen} do
+          {:balance, nil} -> {:error, :falta_cuenta_origen}
+          _ -> {:ok, mapa_opciones}
+        end
+
+      _ ->
+        {:error, :parametro_invalido}
+    end
+  end
 end
--- test/ledger_test.exs
+++ test/ledger_test.exs
@@ -8,4 +8,19 @@ defmodule LedgerTest do
     assert {:ok, :balance} == Parser.validar_subcomando("balance")
     assert {:error, :subcomando_invalido} == Parser.validar_subcomando("otro")
   end
+
+  test "Validar opciones" do
+    assert {:ok, %{archivo_input: "test/fixtures/transacciones.csv"}} = Parser.validar_opciones([], :transacciones)
+    assert {:ok, %{archivo_input: "input.csv"}} = Parser.validar_opciones(["-t=input.csv",], :transacciones)
+    assert {:ok, %{archivo_input: "test/fixtures/transacciones.csv",
+    archivo_output: "output.csv",
+    cuenta_origen: "userA",
+    cuenta_destino: "userB",
+    moneda: "USD"}} = Parser.validar_opciones(["-o=output.csv", "-c1=userA", "-c2=userB", "-m=USD"], :transacciones)
+    assert {:ok, %{cuenta_origen: "userA"}} = Parser.validar_opciones(["-c1=userA"], :balance)
+    assert {:error, :parametro_invalido} = Parser.validar_opciones(["--c1=userA"], :transacciones)
+    assert {:error, :parametro_invalido} = Parser.validar_opciones(["-x=algo"], :transacciones)
+    assert {:error, :parametro_invalido} = Parser.validar_opciones(["dadada"], :transacciones)
+    assert {:error, :falta_cuenta_origen} = Parser.validar_opciones([], :balance)
   end
+end
