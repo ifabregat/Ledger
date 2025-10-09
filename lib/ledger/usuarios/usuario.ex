41a92286ef5c4f4f46f19dc8c733cfe554b4cd79
ifabregat <ifabregat@fi.uba.ar>
2025-10-09 16:29:43 -0300

    Agrego algunas validaciones en usuario

--- lib/ledger/usuarios/usuario.ex
+++ lib/ledger/usuarios/usuario.ex
@@ -2,9 +2,17 @@ defmodule Ledger.Usuarios.Usuario do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @mayor_edad 18
+
   schema "usuarios" do
     field :nombre, :string
     field :fecha_nacimiento, :date
+
+    has_many :transacciones_origen, Ledger.Transacciones.Transaccion,
+      foreign_key: :cuenta_origen_id
+
+    has_many :transacciones_destino, Ledger.Transacciones.Transaccion,
+      foreign_key: :cuenta_destino_id
 
     timestamps()
   end
@@ -13,5 +21,40 @@ defmodule Ledger.Usuarios.Usuario do
     usuario
     |> cast(attrs, [:nombre, :fecha_nacimiento])
     |> validate_required([:nombre, :fecha_nacimiento])
+    |> unique_constraint(:nombre, message: "El nombre ya está en uso")
+    |> validar_mayor_edad()
+    |> validar_cambio_nombre()
+  end
+
+  defp validar_mayor_edad(changeset) do
+    case fetch_field(changeset, :fecha_nacimiento) do
+      {_, fecha_nacimiento} ->
+        edad = div(Date.diff(Date.utc_today(), fecha_nacimiento), 365)
+
+        if edad < @mayor_edad do
+          add_error(changeset, :fecha_nacimiento, "El usuario debe ser mayor de edad")
+        else
+          changeset
+        end
+
+      :error ->
+        changeset
+    end
+  end
+
+  defp validar_cambio_nombre(changeset) do
+    if changeset.data.id do
+      nombre_actual = changeset.data.nombre
+
+      case get_change(changeset, :nombre) do
+        ^nombre_actual ->
+          add_error(changeset, :nombre, "El nuevo nombre debe ser diferente al actual")
+
+        _ ->
+          changeset
+      end
+    else
+      changeset
+    end
   end
 end
