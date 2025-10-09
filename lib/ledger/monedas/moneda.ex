423d88b4281a89c79610ea537673c3d7a02890e6
ifabregat <ifabregat@fi.uba.ar>
2025-10-09 16:37:42 -0300

    Agrego algunas validaciones

--- lib/ledger/monedas/moneda.ex
+++ lib/ledger/monedas/moneda.ex
@@ -6,6 +6,12 @@ defmodule Ledger.Monedas.Moneda do
     field :nombre, :string
     field :precio_dolares, :float
 
+    has_many :transacciones_origen, Ledger.Transacciones.Transaccion,
+      foreign_key: :moneda_origen_id
+
+    has_many :transacciones_destino, Ledger.Transacciones.Transaccion,
+      foreign_key: :moneda_destino_id
+
     timestamps()
   end
 
@@ -13,5 +19,27 @@ defmodule Ledger.Monedas.Moneda do
     moneda
     |> cast(attrs, [:nombre, :precio_dolares])
     |> validate_required([:nombre, :precio_dolares])
+    |> unique_constraint(:nombre, message: "El nombre de la moneda ya está en uso")
+    |> validate_length(:nombre,
+      min: 3,
+      max: 4,
+      message: "El nombre debe tener entre 3 y 4 caracteres"
+    )
+    |> validate_format(:nombre, ~r/^[A-Z]+$/,
+      message: "El nombre debe contener solo letras mayúsculas"
+    )
+    |> validate_number(:precio_dolares,
+      greater_than_or_equal_to: 0.0,
+      message: "El precio en dólares debe ser un número no negativo"
+    )
+    |> validar_inmutabilidad_nombre()
+  end
+
+  defp validar_inmutabilidad_nombre(changeset) do
+    if get_field(changeset, :id) && get_change(changeset, :nombre) do
+      add_error(changeset, :nombre, "El nombre de la moneda no puede ser modificado")
+    else
+      changeset
+    end
   end
 end
