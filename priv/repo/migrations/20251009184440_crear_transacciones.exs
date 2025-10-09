53cb94fe978b3c40a739e8d8af5112dae922f517
ifabregat <ifabregat@fi.uba.ar>
2025-10-09 15:48:10 -0300

    Agrego migracion de transacciones

---
+++ priv/repo/migrations/20251009184440_crear_transacciones.exs
@@ -0,0 +1,22 @@
+defmodule Ledger.Repo.Migrations.CrearTransacciones do
+  use Ecto.Migration
+
+  def change do
+    create tabler(:transacciones) do
+      add :monto, :float, null: false
+      add :tipo, :string, null: false
+
+      add :cuenta_origen_id, references(:usuarios, on_delete: :restrict)
+      add :moneda_origen_id, references(:monedas, on_delete: :restrict)
+      add :cuenta_destino_id, references(:usuarios, on_delete: :restrict), null: false
+      add :moneda_destino_id, references(:monedas, on_delete: :restrict), null: false
+
+      timestamps()
+    end
+
+    create index(:transacciones, [:cuenta_origen_id])
+    create index(:transacciones, [:moneda_origen_id])
+    create index(:transacciones, [:cuenta_destino_id])
+    create index(:transacciones, [:moneda_destino_id])
+  end
+end
