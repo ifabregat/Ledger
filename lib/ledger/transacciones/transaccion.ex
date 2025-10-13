180a9021ccbe78fe5c4edc7f7274a1d1f3682e38
ifabregat <ifabregat@fi.uba.ar>
2025-10-12 21:09:25 -0300

    Cambio cuenta_origen por cuenta_destino en swap

--- lib/ledger/transacciones/transaccion.ex
+++ lib/ledger/transacciones/transaccion.ex
@@ -50,10 +50,16 @@ defmodule Ledger.Transacciones.Transaccion do
 
   def swap_changeset(transaccion, attrs) do
     transaccion
-    |> cast(attrs, [:monto, :tipo, :cuenta_origen_id, :moneda_origen_id, :moneda_destino_id])
-    |> validate_required([:monto, :tipo, :cuenta_origen_id, :moneda_origen_id, :moneda_destino_id])
+    |> cast(attrs, [:monto, :tipo, :cuenta_destino_id, :moneda_origen_id, :moneda_destino_id])
+    |> validate_required([
+      :monto,
+      :tipo,
+      :cuenta_destino_id,
+      :moneda_origen_id,
+      :moneda_destino_id
+    ])
     |> validate_number(:monto, greater_than: 0, message: "El monto debe ser mayor que 0")
-    |> foreign_key_constraint(:cuenta_origen_id)
+    |> foreign_key_constraint(:cuenta_destino_id)
     |> foreign_key_constraint(:moneda_origen_id)
     |> foreign_key_constraint(:moneda_destino_id)
   end
