9d8a222a8db6297fc0f14c5bfe826d38381cd6ff
ifabregat <ifabregat@fi.uba.ar>
2025-10-17 19:04:30 -0300

    Listo las de transacciones

--- test/ledger_test.exs
+++ test/ledger_test.exs
@@ -1,9 +1,15 @@
 defmodule Ledger.UsuariosTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
+  alias Ledger.Repo
   alias Ledger.Usuarios.Usuarios
   alias Ledger.Usuarios.Usuario
   alias Ledger.Monedas.Monedas
   alias Ledger.Monedas.Moneda
+  alias Ledger.Transacciones.Transacciones
+
+  defp nombre_unico_letras(length \\ 3) do
+    for _ <- 1..length, into: "", do: <<Enum.random(?A..?Z)>>
+  end
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Ledger.Repo)
@@ -97,10 +103,6 @@ defmodule Ledger.UsuariosTest do
     assert !changeset.valid?
   end
 
-  defp nombre_unico_letras(length \\ 3) do
-    for _ <- 1..length, into: "", do: <<Enum.random(?A..?Z)>>
-  end
-
   test "crear moneda válida" do
     attrs = %{nombre: nombre_unico_letras(), precio_dolares: Decimal.new("50000")}
     assert {:ok, %Moneda{} = moneda} = Monedas.crear_moneda(attrs)
@@ -158,5 +160,518 @@ defmodule Ledger.UsuariosTest do
 
   test "borrar moneda inexistente" do
     assert {:error, "Moneda no encontrada"} = Monedas.borrar_moneda(-1)
+  end
+
+  test "alta de cuenta" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    attrs = %{
+      cuenta_destino_id: usuario.id,
+      moneda_destino_id: moneda.id,
+      monto: 10.0,
+      tipo: "alta"
+    }
+
+    assert {:ok, _} = Ledger.Transacciones.Transacciones.alta_cuenta(attrs)
+  end
+
+  test "alta de cuenta con moneda ya existente e ingresada" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    attrs = %{
+      cuenta_destino_id: usuario.id,
+      moneda_destino_id: moneda.id,
+      monto: 10.0,
+      tipo: "alta"
+    }
+
+    assert {:ok, _} = Ledger.Transacciones.Transacciones.alta_cuenta(attrs)
+
+    assert {:error, "El usuario ya tiene una cuenta en esta moneda"} ==
+             Ledger.Transacciones.Transacciones.alta_cuenta(attrs)
+  end
+
+  test "alta cuenta sin usuario" do
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    attrs = %{cuenta_destino_id: -1, moneda_destino_id: moneda.id, monto: 10.0, tipo: "alta"}
+
+    assert {:error, "Usuario no encontrado"} ==
+             Ledger.Transacciones.Transacciones.alta_cuenta(attrs)
+  end
+
+  test "alta cuenta sin moneda" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    attrs = %{cuenta_destino_id: usuario.id, moneda_destino_id: -1, monto: 10.0, tipo: "alta"}
+
+    assert {:error, "Moneda no encontrada"} ==
+             Ledger.Transacciones.Transacciones.alta_cuenta(attrs)
+  end
+
+  test "realizar transferencia" do
+    {:ok, usuario_origen} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, usuario_destino} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Sasuke Uchiha #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    {:ok, _} =
+      Ledger.Transacciones.Transacciones.alta_cuenta(%{
+        cuenta_destino_id: usuario_origen.id,
+        moneda_destino_id: moneda.id,
+        monto: 100.0,
+        tipo: "alta"
+      })
+
+    attrs = %{
+      cuenta_origen_id: usuario_origen.id,
+      cuenta_destino_id: usuario_destino.id,
+      moneda_origen_id: moneda.id,
+      tipo: "transferencia",
+      monto: 50.0
+    }
+
+    assert {:ok, _transaccion} = Ledger.Transacciones.Transacciones.realizar_transferencia(attrs)
+  end
+
+  test "realizar transferencia con usuario origen igual a destino" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    attrs = %{
+      cuenta_origen_id: usuario.id,
+      cuenta_destino_id: usuario.id,
+      moneda_origen_id: moneda.id,
+      tipo: "transferencia",
+      monto: 10.0
+    }
+
+    assert {:error, "El usuario origen y destino no pueden ser el mismo"} ==
+             Ledger.Transacciones.Transacciones.realizar_transferencia(attrs)
+  end
+
+  test "realizar swap saldo insuficiente" do
+    {:ok, usuario_origen} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, usuario_destino} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Sasuke Uchiha #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    attrs = %{
+      cuenta_origen_id: usuario_origen.id,
+      cuenta_destino_id: usuario_destino.id,
+      moneda_origen_id: moneda.id,
+      tipo: "transferencia",
+      monto: 50.0
+    }
+
+    assert {:error, "Saldo insuficiente"} ==
+             Ledger.Transacciones.Transacciones.realizar_transferencia(attrs)
+  end
+
+  test "realizar swap" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda_origen} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    {:ok, moneda_destino} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("2")
+      })
+
+    {:ok, _} =
+      Ledger.Transacciones.Transacciones.alta_cuenta(%{
+        cuenta_destino_id: usuario.id,
+        moneda_destino_id: moneda_origen.id,
+        monto: 100.0,
+        tipo: "alta"
+      })
+
+    attrs = %{
+      cuenta_destino_id: usuario.id,
+      moneda_origen_id: moneda_origen.id,
+      moneda_destino_id: moneda_destino.id,
+      tipo: "swap",
+      monto: 50.0
+    }
+
+    assert {:ok, _transaccion} = Ledger.Transacciones.Transacciones.realizar_swap(attrs)
+  end
+
+  test "realizar swap con moneda origen igual a destino" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    attrs = %{
+      cuenta_destino_id: usuario.id,
+      moneda_origen_id: moneda.id,
+      moneda_destino_id: moneda.id,
+      tipo: "swap",
+      monto: 10.0
+    }
+
+    assert {:error, "La moneda de origen y destino no pueden ser la misma"} ==
+             Ledger.Transacciones.Transacciones.realizar_swap(attrs)
+  end
+
+  test "realizar swap con saldo insuficiente" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda_origen} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    {:ok, moneda_destino} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("2")
+      })
+
+    attrs = %{
+      cuenta_destino_id: usuario.id,
+      moneda_origen_id: moneda_origen.id,
+      moneda_destino_id: moneda_destino.id,
+      tipo: "swap",
+      monto: 50.0
+    }
+
+    assert {:error, "Saldo insuficiente para realizar el swap"} ==
+             Ledger.Transacciones.Transacciones.realizar_swap(attrs)
+  end
+
+  test "deshacer transacción alta" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    {:ok, transaccion} =
+      Ledger.Transacciones.Transacciones.alta_cuenta(%{
+        cuenta_destino_id: usuario.id,
+        moneda_destino_id: moneda.id,
+        monto: 50.0,
+        tipo: "alta"
+      })
+
+    assert {:ok, _} =
+             Ledger.Transacciones.Transacciones.deshacer_transaccion(%{id: transaccion.id})
+
+    assert {:error, "Transacción no encontrada"} =
+             Ledger.Transacciones.Transacciones.ver_transaccion(%{id: transaccion.id})
+  end
+
+  test "deshacer transaccion transferencia" do
+    {:ok, u1} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, u2} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Sasuke Uchiha #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    {:ok, _} =
+      Ledger.Transacciones.Transacciones.alta_cuenta(%{
+        cuenta_destino_id: u1.id,
+        moneda_destino_id: moneda.id,
+        monto: 100.0,
+        tipo: "alta"
+      })
+
+    {:ok, transaccion} =
+      Ledger.Transacciones.Transacciones.realizar_transferencia(%{
+        cuenta_origen_id: u1.id,
+        cuenta_destino_id: u2.id,
+        moneda_origen_id: moneda.id,
+        tipo: "transferencia",
+        monto: 50.0
+      })
+
+    assert {:ok, inversa} =
+             Ledger.Transacciones.Transacciones.deshacer_transaccion(%{id: transaccion.id})
+
+    assert inversa.cuenta_origen_id == transaccion.cuenta_destino_id
+    assert inversa.cuenta_destino_id == transaccion.cuenta_origen_id
+    assert inversa.monto == transaccion.monto
+  end
+
+  test "deshacer transaccion no encontrada" do
+    assert {:error, "Transacción no encontrada"} =
+             Ledger.Transacciones.Transacciones.deshacer_transaccion(%{id: -1})
+  end
+
+  test "falla si no es la última transacción" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    {:ok, moneda2} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("2")
+      })
+
+    {:ok, t1} =
+      Transacciones.alta_cuenta(%{
+        cuenta_destino_id: usuario.id,
+        moneda_destino_id: moneda.id,
+        monto: 100.0,
+        tipo: "alta"
+      })
+
+    {:ok, _t2} =
+      Transacciones.realizar_swap(%{
+        cuenta_destino_id: usuario.id,
+        moneda_origen_id: moneda.id,
+        moneda_destino_id: moneda2.id,
+        monto: 50.0,
+        tipo: "swap"
+      })
+
+    assert {:error, "Solo se puede deshacer la última transacción de las cuentas"} =
+             Transacciones.deshacer_transaccion(%{id: t1.id})
+  end
+
+  test "deshacer transaccion swap" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda_origen} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    {:ok, moneda_destino} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("2")
+      })
+
+    {:ok, _} =
+      Ledger.Transacciones.Transacciones.alta_cuenta(%{
+        cuenta_destino_id: usuario.id,
+        moneda_destino_id: moneda_origen.id,
+        monto: 100.0,
+        tipo: "alta"
+      })
+
+    {:ok, transaccion} =
+      Ledger.Transacciones.Transacciones.realizar_swap(%{
+        cuenta_destino_id: usuario.id,
+        moneda_origen_id: moneda_origen.id,
+        moneda_destino_id: moneda_destino.id,
+        tipo: "swap",
+        monto: 50.0
+      })
+
+    assert {:ok, _inversa} =
+             Ledger.Transacciones.Transacciones.deshacer_transaccion(%{id: transaccion.id})
+  end
+
+  test "deshacer transaccion con tipo desconocido" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Test #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    attrs = %{
+      cuenta_destino_id: usuario.id,
+      moneda_destino_id: moneda.id,
+      monto: 100.0,
+      tipo: "rasengan"
+    }
+
+    {:ok, t} = Transacciones.alta_cuenta(attrs)
+
+    {:error, _} = Transacciones.deshacer_transaccion(%{id: t.id})
+  end
+
+  test "deshacer transaccion con tipo ya es un átomo" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    transaccion =
+      %Ledger.Transacciones.Transaccion{
+        cuenta_destino_id: usuario.id,
+        moneda_destino_id: moneda.id,
+        monto: 100.0,
+        tipo: "alta"
+      }
+      |> Ledger.Repo.insert!()
+
+    {:ok, _resultado} = Transacciones.deshacer_transaccion(%{id: transaccion.id})
+
+    assert Repo.get(Ledger.Transacciones.Transaccion, transaccion.id) == nil
+  end
+
+  test "deshacer transaccion sin transacciones en la db" do
+    transaccion_fake = %Ledger.Transacciones.Transaccion{
+      id: 9999,
+      cuenta_origen_id: nil,
+      cuenta_destino_id: 12345
+    }
+
+    assert Ledger.Transacciones.Transacciones.deshacer_transaccion(%{id: transaccion_fake.id}) ==
+             {:error, "Transacción no encontrada"}
+  end
+
+  test "ver transacción existente" do
+    {:ok, usuario} =
+      Ledger.Usuarios.Usuarios.crear_usuario(%{
+        nombre: "Naruto Uzumaki #{System.unique_integer([:positive])}",
+        fecha_nacimiento: ~D[1990-01-01]
+      })
+
+    {:ok, moneda} =
+      Ledger.Monedas.Monedas.crear_moneda(%{
+        nombre: nombre_unico_letras(),
+        precio_dolares: Decimal.new("1")
+      })
+
+    {:ok, transaccion} =
+      Ledger.Transacciones.Transacciones.alta_cuenta(%{
+        cuenta_destino_id: usuario.id,
+        moneda_destino_id: moneda.id,
+        monto: 50.0,
+        tipo: "alta"
+      })
+
+    assert {:ok, ^transaccion} =
+             Ledger.Transacciones.Transacciones.ver_transaccion(%{id: transaccion.id})
+  end
+
+  test "ver transacción inexistente" do
+    assert {:error, "Transacción no encontrada"} =
+             Ledger.Transacciones.Transacciones.ver_transaccion(%{id: -1})
   end
 end
