1508d1012d8f58eff8833c7885a2d330fc21086e
ifabregat <ifabregat@fi.uba.ar>
2025-10-17 15:47:06 -0300

    Configuro para las pruebas

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,9 @@ config :ledger, Ledger.Repo,
   database: "ledger",
   username: "admin",
   password: "admin123",
-  hostname: "localhost"
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
 
 config :ledger, ecto_repos: [Ledger.Repo]
 config :ledger, Ledger.Repo, log: false
