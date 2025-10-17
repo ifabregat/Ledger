0d7c8bb33783860d056de243a7a0736b23363675
ifabregat <ifabregat@fi.uba.ar>
2025-10-17 16:29:22 -0300

    Listo las pruebas de moneda y monedas

--- test/ledger_test.exs
+++ test/ledger_test.exs
@@ -2,6 +2,8 @@ defmodule Ledger.UsuariosTest do
   use ExUnit.Case, async: true
   alias Ledger.Usuarios.Usuarios
   alias Ledger.Usuarios.Usuario
+  alias Ledger.Monedas.Monedas
+  alias Ledger.Monedas.Moneda
 
   setup do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Ledger.Repo)
@@ -13,7 +15,7 @@ defmodule Ledger.UsuariosTest do
     Ecto.Changeset.traverse_errors(changeset, fn {msg, _opts} -> msg end)
   end
 
-  test "crea un usuario válido" do
+  test "crear usuario válido" do
     nombre = "Naruto Uzumaki #{System.unique_integer([:positive])}"
     attrs = %{nombre: nombre, fecha_nacimiento: ~D[1990-01-01]}
 
@@ -21,7 +23,7 @@ defmodule Ledger.UsuariosTest do
     assert usuario.nombre == nombre
   end
 
-  test "falla si el nombre ya existe" do
+  test "crear usuario con nombre ya existente" do
     nombre = "Naruto Uzumaki #{System.unique_integer([:positive])}"
     attrs = %{nombre: nombre, fecha_nacimiento: ~D[1990-01-01]}
     {:ok, _} = Usuarios.crear_usuario(attrs)
@@ -93,5 +95,68 @@ defmodule Ledger.UsuariosTest do
     usuario = %Usuario{id: 1, nombre: "Kakashi"}
     changeset = Usuario.changeset(usuario, %{})
     assert !changeset.valid?
+  end
+
+  defp nombre_unico_letras(length \\ 3) do
+    for _ <- 1..length, into: "", do: <<Enum.random(?A..?Z)>>
+  end
+
+  test "crear moneda válida" do
+    attrs = %{nombre: nombre_unico_letras(), precio_dolares: Decimal.new("50000")}
+    assert {:ok, %Moneda{} = moneda} = Monedas.crear_moneda(attrs)
+    assert moneda.nombre == attrs.nombre
+  end
+
+  test "crear moneda con nombre ya existente" do
+    nombre = nombre_unico_letras()
+    attrs = %{nombre: nombre, precio_dolares: Decimal.new("3500")}
+    {:ok, _} = Monedas.crear_moneda(attrs)
+
+    {:error, changeset} = Monedas.crear_moneda(attrs)
+    assert %{nombre: [_]} = ecto_errores(changeset)
+  end
+
+  test "editar moneda válida" do
+    nombre = nombre_unico_letras()
+    {:ok, moneda} = Monedas.crear_moneda(%{nombre: nombre, precio_dolares: Decimal.new("2")})
+    {:ok, updated} = Monedas.editar_moneda(moneda.id, %{precio_dolares: Decimal.new("2.5")})
+    assert updated.precio_dolares == Decimal.new("2.5")
+  end
+
+  test "editar moneda intentando cambiar nombre" do
+    nombre = nombre_unico_letras()
+    {:ok, moneda} = Monedas.crear_moneda(%{nombre: nombre, precio_dolares: Decimal.new("150")})
+    {:error, changeset} = Monedas.editar_moneda(moneda.id, %{nombre: "XRP"})
+    assert %{nombre: [_]} = ecto_errores(changeset)
+  end
+
+  test "editar moneda inexistente" do
+    assert {:error, "Moneda no encontrada"} =
+             Monedas.editar_moneda(-1, %{precio_dolares: Decimal.new("1")})
+  end
+
+  test "ver moneda existente" do
+    nombre = nombre_unico_letras()
+    {:ok, moneda} = Monedas.crear_moneda(%{nombre: nombre, precio_dolares: Decimal.new("35")})
+
+    {:ok, moneda_leida} = Monedas.ver_moneda(moneda.id)
+    assert moneda_leida.id == moneda.id
+    assert moneda_leida.nombre == moneda.nombre
+    assert Decimal.compare(moneda_leida.precio_dolares, moneda.precio_dolares) == :eq
+  end
+
+  test "ver moneda inexistente" do
+    assert {:error, "Moneda no encontrada"} = Monedas.ver_moneda(-1)
+  end
+
+  test "borrar moneda sin transacciones" do
+    nombre = nombre_unico_letras()
+    {:ok, moneda} = Monedas.crear_moneda(%{nombre: nombre, precio_dolares: Decimal.new("180")})
+    assert {:ok, _} = Monedas.borrar_moneda(moneda.id)
+    assert {:error, "Moneda no encontrada"} = Monedas.ver_moneda(moneda.id)
+  end
+
+  test "borrar moneda inexistente" do
+    assert {:error, "Moneda no encontrada"} = Monedas.borrar_moneda(-1)
   end
 end
